f89757da09b4d228852440aca285382b8b316528
Ana Ortiz <ana999orz@gmail.com>
2026-06-20 14:31:59 -0300

    vista inicio y formulario de reclamo

--- reclamos/urls.py
+++ reclamos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('registrar/', views.registrar_reclamo, name='registrar_reclamo'),
 ]
--- reclamos/views.py
+++ reclamos/views.py
@@ -1,39 +1,8 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.models import User
-
-from .models import Reclamo, Estado
-from .forms import ReclamoForm, ConsultaReclamoForm
-
+from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'reclamos/inicio.html')
 
-
 def registrar_reclamo(request):
+    return render(request, 'reclamos/registrar_reclamo.html')
 
-    if request.method == 'POST':
-        form = ReclamoForm(request.POST)
-
-        if form.is_valid():
-
-            reclamo = form.save(commit=False)
-
-            reclamo.usuario = User.objects.first()
-
-            reclamo.estado = Estado.objects.get(nombre='Nuevo')
-
-            reclamo.save()
-
-            return redirect(
-                'confirmacion_reclamo',
-                reclamo_id=reclamo.id
-            )
-
-    else:
-        form = ReclamoForm()
-
-    return render(
-        request,
-        'reclamos/registrar_reclamo.html',
-        {'form': form}
-    )
