46f1c2710586699ff049a1b1c3b72f68b7d77de8
Ana Ortiz <ana999orz@gmail.com>
2026-06-21 15:23:19 -0300

    se mejoro toda la vista de pagina reclamos v1 lista

--- reclamos/views.py
+++ reclamos/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .forms import ReclamoForm
+
 
 def inicio(request):
     return render(request, 'reclamos/inicio.html')
 
-def registrar_reclamo(request):
-    return render(request, 'reclamos/registrar_reclamo.html')
 
+def registrar_reclamo(request):
+
+    form = ReclamoForm()
+
+    return render(
+        request,
+        'reclamos/registrar_reclamo.html',
+        {'form': form}
+    )
